043c131ee0c8783f8237cd23b049cb4b13f072b2
Andrey Yaromenok <ayaromenok@gmail.com>
2020-10-07 13:07:33 +0300

    #2 use wget instead of doenload_models.py due to crash on arm

--- cv/dnn/bin/_run_YOLO4.sh
+++ cv/dnn/bin/_run_YOLO4.sh
@@ -6,8 +6,21 @@ export OPENCV_TEST_DATA_PATH=$(echo ~/sdk/src/opencv/opencv_extra/testdata)
 echo $OPENCV_TEST_DATA_PATH
 cd ~/sdk/src/opencv/opencv_extra/testdata/dnn
 pwd
-python3 ./download_models.py YOLOv4-tiny
-python3 ./download_models.py YOLOv4
+#crash on arm - usw wget instead
+#python3 ./download_models.py YOLOv4-tiny
+#python3 ./download_models.py YOLOv4
+
+if test -f "./yolov4.weights"; then
+echo "YOLOv4 weights is OK"
+else
+    wget https://github.com/AlexeyAB/darknet/releases/download/darknet_yolo_v3_optimal/yolov4.weights
+fi
+if test -f "./yolov4-tiny.weights"; then
+echo "YOLOv4-tiny weights is OK"
+else
+wget https://github.com/AlexeyAB/darknet/releases/download/darknet_yolo_v4_pre/yolov4-tiny.weights
+fi
+
 cd $CUR_DIR
 pwd
 ls
